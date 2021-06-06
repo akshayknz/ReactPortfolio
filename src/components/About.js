f0a2fde1b0670e0adb6487286cb725812b1b0259
Akshay K Nair <akshayakn6@gmail.com>
2021-06-06 21:01:40 +0530

    dfrg thrtg

--- src/components/About.js
+++ src/components/About.js
@@ -22,7 +22,7 @@ const About = () => {
   return (
     <div>
       <h1>About US</h1>
-      <p>About US page body content</p>
+      <p>About US page body contentYo baby</p>
       <motion.div className="single" initial="exit" animate="enter" exit="exit">
         <motion.img
           variants={imageVariants}
